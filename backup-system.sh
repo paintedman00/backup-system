9ba95ce418227dfcfc16ea27d0f9cec61e8298aa
Soroush Mousa Rezaei <38794885+paintedman00@users.noreply.github.com>
2024-06-08 23:00:41 +0200

    Update backup-system.sh

--- backup-system.sh
+++ backup-system.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
-function createBackup() {
+# Function to create a backup
+create_backup() {
   local backup_dir=$1
   local date=$(date +%Y-%m-%d)
   local backup_file="$backup_dir/backup-$date.tar.gz"
 
-  tar -czf $backup_file /
+  tar -czf "$backup_file" /
 }
 
-# Get the backup directory 
+# Prompt for the backup directory
 read -p "Enter the name of the backup directory: " backup_dir
 
-# Create backup
-createBackup $backup_dir
+# Create the backup
+create_backup "$backup_dir"
 
+# Display a confirmation message
 echo "The backup has been created."
